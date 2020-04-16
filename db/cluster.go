34cdd4c6cd5940d169682cc4043e9ff82fcd228e
Noah  Florin-Sefton <noah@otis-app.com>
2020-04-16 23:35:57 +0100

    Creating an aurora-postgresql db cluster

--- db/cluster.go
+++ db/cluster.go
@@ -12,22 +12,33 @@ type rdsClient struct {
 	*client.RDSClient
 }
 
+func getEnvOrPanic( key string ) string {
+	v, exists := os.LookupEnv( key )
+	if !exists {
+		panic("Environment variabl not found: " + key)
+	}
+	return v
+}
+
 
 func (c* rdsClient) createCluster() (*rds.CreateDBClusterOutput, error) {
 	input := &rds.CreateDBClusterInput{
 		AvailabilityZones: []*string{
 			aws.String(os.Getenv("DB_CLUSTER_ZONE")),
 		},
-		BackupRetentionPeriod:       aws.Int64(1),
-		DBClusterIdentifier:         aws.String(os.Getenv("DB_CLUSTER_NAME")),
-		DBClusterParameterGroupName: aws.String("mydbclusterparametergroup"),
-		DatabaseName:                aws.String(os.Getenv("DB_CLUSTER_ZONE")),
-		Engine:                      aws.String("aurora"),
-		EngineVersion:               aws.String("5.6.10a"),
-		MasterUserPassword:          aws.String("mypassword"),
-		MasterUsername:              aws.String("myuser"),
-		Port:                        aws.Int64(3306),
-		StorageEncrypted:            aws.Bool(true),
+		BackupRetentionPeriod:       	 aws.Int64(1),
+		DBClusterIdentifier:         	 aws.String(getEnvOrPanic("DB_CLUSTER_NAME")),
+		DBClusterParameterGroupName: 	 aws.String(getEnvOrPanic("DB_NAME")),
+		DatabaseName:                	 aws.String(getEnvOrPanic("DB_CLUSTER_ZONE")),
+		Engine:                      	 aws.String("aurora-postgresql"),
+		EngineVersion:                	 aws.String("10.7"),
+		EngineMode:						 aws.String("serverless"),
+		MasterUserPassword:              aws.String(getEnvOrPanic("DB_MASTER_PASSWORD")),
+		MasterUsername:                  aws.String(getEnvOrPanic("DB_MASTER_USERNAME")),
+		StorageEncrypted:                aws.Bool(true),
+		EnableHttpEndpoint:			     aws.Bool( true ),
+		EnableIAMDatabaseAuthentication: aws.Bool( true ),
+
 	}
 
 	return c.Client.CreateDBCluster(input)
--- env.sh
+++ env.sh
@@ -4,4 +4,4 @@ export DB_CLUSTER_NAME=Otis-cluster-1
 export DB_NAME=Otis-db-1
 export DB_ZONE=eu-west-2
 export DB_MASTER_USERNAME=otis-db-master
-export DB_MASTER_PASSWORD=MWH!q9GG903r
+export DB_MASTER_PASSWORD=ZkXzi2J5SN$S
