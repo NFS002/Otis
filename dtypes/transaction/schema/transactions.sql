912e44197b03273cef805b37ef7f3249c675bf78
Noah  Florin-Sefton <noah@otis-app.com>
2020-04-22 19:04:40 +0100

    Transactions postgressql schema

--- dtypes/transaction/schema/transactions.sql
+++ dtypes/transaction/schema/transactions.sql
@@ -1,3 +1,18 @@
-CREATE TABLE IF NOT EXISTS transactions (
+-- gender enum data type
+create type TRANSACTION_T as enum('DEBIT', 'CREDIT');
 
+CREATE TABLE IF NOT EXISTS transactions (
+    id                      SERIAL,
+    created_at              DATE            not null,
+    created_at_time         TIMESTAMP       ,
+    processed_time          TIMESTAMP,
+    outlet_id               INTEGER,
+    value                   DECIMAL,
+    currency                VARCHAR(5)      not null,
+    is_online               BOOLEAN,
+    is_contactless          BOOLEAN,
+    type                    TRANSACTION_T   not null,
+    offer_id                INTEGER,
+
+    CHECK ( value > 0 )
 )
