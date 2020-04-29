ef653dda6cf47fd5b96264fdc093c028dd9b652c
Noah  Florin-Sefton <noah@otis-app.com>
2020-04-29 18:38:31 +0100

    SQL schemas for groups, groupmembers, and groupadmins

--- dtypes/groups/schema/groupmembers.sql
+++ dtypes/groups/schema/groupmembers.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS groupmembers (
 
         CONSTRAINT dulplicate_members UNIQUE (group_id, user_id),
 
-        CONSTRAINT  pk_groupmembers_id          PRIMARY KEY (group_id),
-        CONSTRAINT  fk_groumembers_id           FOREIGN KEY (group_id)  REFERENCES groups (id) ON UPDATE CASCADE ON DELETE RESTRICT,
-        CONSTRAINT  fk_groumembers_user_id      FOREIGN KEY (user_id)   REFERENCES users (id)  ON UPDATE CASCADE ON DELETE RESTRICT
+        CONSTRAINT  pk_groupmembers_id         PRIMARY KEY (group_id),
+        CONSTRAINT  fk_groumembers_id          FOREIGN KEY (group_id)  REFERENCES groups (id) ON UPDATE CASCADE ON DELETE RESTRICT,
+        CONSTRAINT  fk_groumembers_user_id     FOREIGN KEY (user_id)   REFERENCES users (id)  ON UPDATE CASCADE ON DELETE RESTRICT
 );
