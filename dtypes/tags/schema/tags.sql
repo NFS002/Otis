fc6f9c9699c3437433989941cb74e74af0efc73c
Noah  Florin-Sefton <noah@otis-app.com>
2020-05-25 15:06:59 +0100

    Updated infrastructure/deployment

--- dtypes/tags/schema/tags.sql
+++
@@ -1,8 +0,0 @@
--- tags table schema
-CREATE TABLE IF NOT EXISTS tags (
-         name                    VARCHAR               not null  unique, -- ISO 3166-1 alpha-2
-         info                    VARCHAR,
-         created_at              DATE                  not null  default CURRENT_DATE,
-
-         CONSTRAINT              pk_tags_name          PRIMARY KEY (name)
-);
--- service/lib/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Utility functions for various purposes"""
-from .conf import *
-from .formatter import LogFormatter
-from .logs import *
-from .wrappers import *
