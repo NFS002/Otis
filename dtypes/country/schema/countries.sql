89941cc50b8d4f5ea1577d929e5a296fe9fb830c
Noah  Florin-Sefton <noah@otis-app.com>
2020-05-26 13:21:15 +0100

    Updated infrastructure/deployment

--- dtypes/country/schema/countries.sql
+++
@@ -1,7 +0,0 @@
--- countries table schema
-CREATE TABLE IF NOT EXISTS countries (
-    code                    VARCHAR(4)            not null  unique, -- ISO 3166-1 alpha-2
-    name                    VARCHAR               not null  unique,
-
-    CONSTRAINT              pk_countries_code     PRIMARY KEY (code)
-);
--- dtypes/transaction/weather.go
+++
@@ -1,7 +0,0 @@
-package transaction
-
-// Weather : Struct is a local representation of the weather at the time of transaction
-// Holiday resources retrieved from the DB are serialized to an instance of this struct
-type Weather struct {
-
-}
